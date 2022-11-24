2aa52c73e21316fb63d57750a37161b7021bfe45
Cody Burley <codyburley@gmail.com>
2022-11-24 15:58:40 -0500

    started card styling

--- src/components/PokedexEntry/PokedexEntry.js
+++ src/components/PokedexEntry/PokedexEntry.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './PokedexEntry.scss'
 
 const PokedexEntry = ({ entry_number, pokemon_species }) => {
-  const capitalName = pokemon_species.name.charAt(0).toUpperCase() + pokemon_species.name.slice(1);
+  // const capitalName = pokemon_species.name.charAt(0).toUpperCase() + pokemon_species.name.slice(1);
+  const entryNumber = String(entry_number).padStart(3, 0);
+
   return (
     <article className='entry'>
       <div className='entry__container'>
-        {entry_number}
-        {capitalName}
+        No.
+        {entryNumber}
       </div>
       <img
         className='entry__image'
